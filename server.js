b3f08cea4fa09ed5067f9df8525168969e00cd87
Cameron <cameron.karasiuk@gmail.com>
2022-05-26 03:05:42 -0400

    front end implemented

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
 //middleware
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
@@ -120,6 +121,22 @@ function validateAnimal(animal) {
     }
     return true;
 }
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log('API server now on port 3001!');
